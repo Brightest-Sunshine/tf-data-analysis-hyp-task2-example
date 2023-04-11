140b68e702ad038ad523134daa033b83a032b946
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2023-04-11 16:55:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 
 chat_id = 453878141 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    samples = [x, y]
-    result = anderson_ksamp(samples)
-    alpha = 0.05
-    if result.statistic < result.critical_values[2]:
-        return False
-    else:
+    stat, pvalue = MMD(compute_kernel="laplacian", gamma=1).test(x, y)
+    alpha = 0.05 
+    if pvalue < alpha:
         return True
+    else:
+        return False
