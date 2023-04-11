7fe7591ebca949da4d55a4abe2296ad86d3bd28d
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2023-04-11 14:59:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 453878141 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    samples = [x, y]
+    result = anderson_ksamp(samples)
+    alpha = 0.05
+    if result.statistic < result.critical_values[2]:
+        return False
+    else:
+        return True
